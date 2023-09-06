81407d101d9a74cc4f13c66595ba03d840933596
mike.stackhouse <mike.stackhouse@atorusresearch.com>
2023-09-06 18:27:41 +0000

    Additional error generators

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,4 @@
-#' Input validator to ensure object is a datasetjson object
+#' Input validator to ensure object is a proper package object
 #'
 #' @param x Input object to check
 #'
@@ -7,5 +7,23 @@
 stopifnot_datasetjson <- function(x) {
   if (!inherits(x, "datasetjson")) {
     stop("Input must be a datasetjson object", call.=FALSE)
+  }
+}
+
+stopifnot_dataset_metadata <- function(x) {
+  if (!inherits(x, "dataset_metadata")) {
+    stop("Input must be a dataset_metadata object", call.=FALSE)
+  }
+}
+
+stopifnot_data_metadata <- function(x) {
+  if (!inherits(x, "data_metadata")) {
+    stop("Input must be a data_metadata object", call.=FALSE)
+  }
+}
+
+stopifnot_file_metadata <- function(x) {
+  if (!inherits(x, "file_metadata")) {
+    stop("Input must be a file_metadata object", call.=FALSE)
   }
 }
