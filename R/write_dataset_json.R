bbea8a4982958b54c1cd1791bb8df0ec9a8704d3
mike.stackhouse <mike.stackhouse@atorusresearch.com>
2023-09-09 20:25:35 +0000

    Complete writer

--- R/write_dataset_json.R
+++ R/write_dataset_json.R
@@ -1,1 +1,49 @@
-# Writer for a Dataset JSON
+#' Write out a Dataset JSON file
+#'
+#' @param x datasetjson object
+#' @param file File path to save Dataset JSON file
+#' @param pretty If TRUE, write with readable formatting
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' # TODO:
+write_dataset_json <- function(x, file, pretty=TRUE) {
+  stopifnot_datasetjson(x)
+
+  # Populate the as-of datetime
+  x[['asOfDateTime']] <- get_datetime()
+
+  if (!missing(file)) {
+    # Make sure the output path exists
+    if(!dir.exists(dirname(file))) {
+      stop("Folder supplied to `file` does not exist", call.=FALSE)
+    }
+
+    # Attach the file OID
+    x <- set_file_oid(x, tools::file_path_sans_ext(file))
+
+    # Write file to disk
+    cat(
+      jsonlite::toJSON(
+        x,
+        dataframe = "values",
+        na = "null",
+        auto_unbox = TRUE,
+        pretty = pretty),
+      "\n", file = path
+      )
+  } else {
+    # Untested
+    cat(
+      jsonlite::toJSON(
+        x,
+        dataframe = "values",
+        na = "null",
+        auto_unbox = TRUE,
+        pretty = pretty),
+      "\n"
+      )
+  }
+}
