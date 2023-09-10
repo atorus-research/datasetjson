994ae2627375ba2d3a46648ec39b771b086d9908
mike.stackhouse <mike.stackhouse@atorusresearch.com>
2023-09-10 21:43:19 +0000

    Add functions to extract the character array column names

--- R/date_time_conversions.R
+++ R/date_time_conversions.R
@@ -59,3 +59,37 @@ convert_to_sas_datetimenum <- function(x) {
   x[col_names] <- lapply(get_datetime_cols(x) - (365 * 24 * 60 * 60 * 10), as.numeric)
   x
 }
+
+#' Check if a list has an element named displayFormat with a desired value
+#'
+#' This function checks if a list has the element displayFormat, and if the
+#' value of that element is within a list of specific values provided in a
+#' character array. This is intended to work with the datasetjson package arrays
+#' `sas_date_formats`, `sas_datetime_formats`, and `sas_time_formats`.
+#'
+#' @param x The items element list of a Dataset JSON file
+#' @param formats A character array
+#'
+#' @return Boolean vector
+#' @noRd
+has_format <- function(x, formats) {
+  !is.null(x$displayFormat) && x$displayFormat %in% formats
+}
+
+#' Get the names of columns from a dataset that have specific formats
+#'
+#' This function will take the items element of a Dataset JSON file and return
+#' then names of columns that have a value within the specified character array
+#' of values.
+#'
+#' @param x The items element list of a Dataset JSON file
+#' @param formats A character array
+#'
+#' @return A character vector
+#' @noRd
+get_format_cols <- function(x, formats) {
+  # Filter the provided list to elements that have the specified array
+  # of displayFormat values, then extract the name columns from
+  # that subset list
+  vapply(Filter(function(X) has_format(X, formats), x), function(X) X$name, "")
+}
