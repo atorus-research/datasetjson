300029925490f82a72a50b53a092b3450ef12e61
mike.stackhouse <mike.stackhouse@atorusresearch.com>
2023-09-11 20:16:56 +0000

    Add imports

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,8 @@
+#' @importFrom jsonvalidate json_validate
+#' @importFrom jsonlite fromJSON toJSON
+#' @importFrom tools file_path_sans_ext
+NULL
+
 #' @keywords internal
 #' @family internal
 "_PACKAGE"
