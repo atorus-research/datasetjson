a0026f866a7f788e844d8d6c5f7f3668d863d401
mike.stackhouse <mike.stackhouse@atorusresearch.com>
2023-09-12 15:49:03 +0000

    reorderd the iris_items to match CDISC spec

--- data-raw/iris_items.R
+++ data-raw/iris_items.R
@@ -1,11 +1,11 @@
 ## code to prepare `iris_items` dataset
 iris_items <- tibble::tribble(
-  ~OID,                 ~name,          ~label,           ~type,    ~length,     ~keySequence, ~displayFormat,
-  'IT.IR.Sepal.Length', 'Sepal.Length', 'Sepal Length',   'float',  NA_integer_, 2L,           NA_character_,
-  'IT.IR.Sepal.Width',  'Sepal.Width',  'Sepal Width',    'float',  NA_integer_, NA_integer_,  NA_character_,
-  'IT.IR.Petal.Length', 'Petal.Length', 'Petal Length',   'float',  NA_integer_, 3L,           NA_character_,
-  'IT.IR.Petal.Width',  'Petal.Width',  'Petal Width',    'float',  NA_integer_, NA_integer_,  NA_character_,
-  'IT.IR.Species',      'Species',      'Flower Species', 'string', 10L,         1L,           NA_character_
+  ~OID,                 ~name,          ~label,           ~type,    ~length,     ~displayFormat, ~keySequence,
+  'IT.IR.Sepal.Length', 'Sepal.Length', 'Sepal Length',   'float',  NA_integer_, NA_character_,  2L,
+  'IT.IR.Sepal.Width',  'Sepal.Width',  'Sepal Width',    'float',  NA_integer_, NA_character_,  NA_integer_,
+  'IT.IR.Petal.Length', 'Petal.Length', 'Petal Length',   'float',  NA_integer_, NA_character_,  3L,
+  'IT.IR.Petal.Width',  'Petal.Width',  'Petal Width',    'float',  NA_integer_, NA_character_,  NA_integer_,
+  'IT.IR.Species',      'Species',      'Flower Species', 'string', 10L,         NA_character_, 1L
 )
 
 usethis::use_data(iris_items, overwrite = TRUE)
