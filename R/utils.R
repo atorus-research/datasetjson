d3539a0e645d7e3743e38267e751075952eb9651
mstackhouse <Mike.Stackhouse@atorusresearch.com>
2025-01-22 14:45:35 -0500

    We need POSIXlt on right to check timezone attributes, so I want read/write to be consistent

--- R/utils.R
+++ R/utils.R
@@ -102,7 +102,7 @@ date_time_conversions <- function(d, dt, tdt){
   time_cols <- dt %in% c("time") & tdt %in% "integer"
   d[date_cols] <- lapply(d[date_cols], as.Date, tz = "UTC")
   d[datetime_cols] <- lapply(d[datetime_cols],
-                             as.POSIXct,
+                             as.POSIXlt,
                              tz = "UTC",
                              tryFormats = "%Y-%m-%dT%H:%M:%S")
   d[time_cols] <- lapply(d[time_cols], hms)
